df5681136bf4eedd0fb9441a238c6e842c12ef64
Lyba Lila <lila.lyba5@yandex.ru>
2023-04-04 21:41:59 +0500

    create model and schema user

---
+++ server/src/models/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from src.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    hashed_password = Column(String)
+    name = Column(String, unique=True, index=True)
+
+    # items = relationship("Item", back_populates="owner")
