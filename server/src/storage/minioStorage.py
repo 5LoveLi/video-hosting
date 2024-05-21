5aedd025449a8fa42e8f3c41776d63f424427e86
luba <lilaluba8@gmail.com>
2024-05-21 22:00:12 +0500

    edit pass minio

--- server/src/storage/minioStorage.py
+++ server/src/storage/minioStorage.py
@@ -7,7 +7,7 @@ load_dotenv('.env')
 
 
 client = Minio(
-    "127.0.0.1:9091",
+    os.getenv('PASS_MINIO'),
     access_key=os.getenv('ACCESS_KEY_MINIO'),
     secret_key=os.getenv('SECRET_KEY_MINIO'),
     secure=False,
@@ -17,12 +17,12 @@ def upload_video_minio(file, file_name):
     video_bytes = io.BytesIO(file)
     path = file_name + '.mp4'
     client.put_object('hosting', path, video_bytes, len(file))
-    return 'http://127.0.0.1:9091/hosting/' + path +'?'
+    return 'http://' + os.getenv('PASS_MINIO') + '/hosting/' + path +'?'
 
 
 def upload_preview_minio(file, name_file):
     bytes_preview = io.BytesIO(file)
     path = name_file + '.jpg'
     client.put_object('hosting', path, bytes_preview, len(file))
-    return 'http://127.0.0.1:9091/hosting/' + path +'?'
+    return 'http://' + os.getenv('PASS_MINIO') + '/hosting/' + path +'?'
 
